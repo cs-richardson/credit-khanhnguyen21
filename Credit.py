f658c96640c0495c98e2909bbc3f3dfb3474ab96
Khanh Nguyen <khnguyen21@ssis.edu.vn>
2019-09-23 14:51:10 +0700

    Update

--- Credit.py
+++ Credit.py
@@ -11,13 +11,6 @@ if i == 15 or i == 13:
         else:
             number = number * 2
             total = total + int(number % 10) + number // 10
-    if total % 10 == 0:
-        if i == 13:
-            print('Visa')
-        else:
-            print('AmEx')
-    else:
-        print('Invalid')
 elif i == 16:
     for x in range (i):
         number = int(cred % 10)
@@ -27,13 +20,17 @@ elif i == 16:
             total = total + int(number % 10) + number // 10
         else:
             total = total + number
-    if total % 10 == 0:
-            if int(creditNum / 1000000000000000) == 4:
-                print('Visa')
-            else:
-                print('MasterCard')
-    else:
-        print('Invalid')
+if total % 10 == 0:
+        if i == 13:
+            print('Visa')
+        elif i == 15:
+            print('AmEx')
+        elif int(creditNum / 1000000000000000) == 4:
+            print('Visa')
+        elif int(creditNum / 100000000000000) >= 51 and int(creditNum / 100000000000000) <= 55 :
+            print('MasterCard')
+        else:
+            print('Invalid')
 else:
     print('Invalid')
         
